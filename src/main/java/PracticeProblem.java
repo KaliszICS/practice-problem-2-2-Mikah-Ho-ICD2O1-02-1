e4e814d7e2307d2e2feff4767a27d2dc8a2bb8f8
Mikah-Ho-ICD2O1-02 <mikah.ho@student.tdsb.on.ca>
2026-03-11 17:47:43 +0000

    sjgvsdv

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,48 @@
+/**
+ * File: More Functions
+ * Author: Mikah Ho
+ * Date Created: Mar 11, 2026
+ * Date Last Modified: Mar 11, 2026
+ */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+
+	}
+
+	//q1
+	public static boolean isEven(int num) {
+
+		return num % 2 == 0;
+
+	}
+
+	//q2
+	public static boolean isOdd(int num) {
+
+		return num % 2 != 0;
+
+	}
+
+	//q3
+	public static boolean isPositive(int num) {
+
+		return num > 0;
+
+	}
+
+	//q4
+	public static boolean isNegative(int num) {
+
+		return num < 0;
+
+	}
+
+	//q5
+	public static int combinedLength(String word1, String word2) {
+
+		return (word1 + word2).length();
 
 	}
 
